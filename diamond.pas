a38b142a38e5888dcba784b921599d894207d416
SaaFomenko <s.fomenko@list.ru>
2018-04-30 02:06:14 +0300

    The source code and the print routine of a rhomb from lessons of Pascal. Asks the rhomb height which shall be odd and the positive.

---
+++ diamond.pas
@@ -0,0 +1,39 @@
+program diamond;
+var
+    n, k, h, i: integer;
+begin
+    { input of corect number by the user }
+    repeat
+        write('Enter the diamond height (positive odd): ');
+        readln(h)
+    until (h > 0) and (h mod 2 = 1);
+    n := h div 2;
+    { print of the top part of a figure }
+    for k := 1 to n + 1 do
+    begin
+        for i := 1 to n + 1 - k do
+            write(' ');
+        write('*');
+        if k > 1 then
+        begin
+            for i := 1 to 2*k - 3 do
+                write(' ');
+            write('*')
+        end;
+        writeln
+    end;
+    { print bottom part }
+    for k := n downto 1 do
+    begin
+        for i := 1 to n + 1 - k do
+            write(' ');
+        write('*');
+        if k > 1 then 
+        begin
+            for i := 1 to 2*k - 3 do
+                write(' ');
+            write('*')
+        end;
+        writeln
+    end
+end.    
